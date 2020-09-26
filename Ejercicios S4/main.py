7a68bd301c7aa3c27df3dd387e5278ab350497d0
Jonathan Rojas Alfaro <jonathan_ro_alfa@outlook.com>
2020-09-25 19:03:22 -0600

    Reorganized files and completed  exercise 3.6

--- Ejercicios S4/main.py
+++ Ejercicios S4/main.py
@@ -1,1 +1,23 @@
-print("Hola")
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+def histEq():
+    img = cv.imread("1.jpg")
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    hist, bins = np.histogram(img.flatten(), 256, [0, 256])
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * float(hist.max()) / cdf.max()
+    plt.plot(cdf_normalized, color='b')
+    plt.hist(img.flatten(), 256, [0, 256], color='r')
+    plt.xlim([0, 256])
+    plt.legend(('cdf', 'histogram'), loc='upper left')
+    plt.show()
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+    cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+    img2 = cdf[img]
+    cv.imwrite("1HEQ.jpg", img2)
+    cv.waitKey()
+
+histEq()
